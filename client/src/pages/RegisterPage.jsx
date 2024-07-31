51787c05cd8ed3a07b697f1648f8916629220316
jolgo <jlgo60@hotmail.com>
2024-07-31 05:10:58 -0500

    se instalo cors

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import { registerRequest } from "../api/auth";
 
 const RegisterPage = () => {
   const { register, handleSubmit } = useForm();
@@ -6,10 +7,11 @@ const RegisterPage = () => {
   return (
     <div className="bg-zinc-800 max-w-md p-10 rounded-md">
       <form
-        onSubmit={handleSubmit((values) => {
+        onSubmit={handleSubmit(async (values) => {
           console.log(values);
+          const res = await registerRequest(values);
+          console.log(res);
         })}
-        action=""
       >
         <input
           type="text"
--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 import express from 'express'
 import morgan from 'morgan'
 import cookiesParser from 'cookie-parser'
+import cors from 'cors'
+
+
 import autRoutes from './routes/auth.routes.js'
 import taskRoutes from './routes/tasks.routes.js'
 
 const app = express()
 // Para poder ejecutar esta linea de codigo (const cookies = req.cookies), la cual esta en validateToken.js debes importar el middlewares cookiesParser
+
+// Configurar y habilitar CORS para permitir solicitudes desde 'http://localhost:5173'
+app.use(cors({
+    origin: 'http://localhost:5173', // Especificar el origen permitido
+}));
+
 app.use(morgan("dev"))
 app.use(express.json())//!Este middlewhere debes colocarlo para que  muestre las peticiones RECUERDA LO QUE PASO EN LA U
 app.use(cookiesParser())
+
+
 
 
 app.use("/api", autRoutes)
