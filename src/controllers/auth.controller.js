f7417283564d07af66eab564e621c065d4f275e4
Jorge <jlgo60@hotmail.com>
2024-11-05 05:30:46 -0500

    Se creo funcionalidades comprobar si un usuario existe en login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,6 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs'//Modulo para encriptar contraseña
-import { createAccesToken } from '../libs/jwt.js'//Modulo para encriptar contraseña
-
+import { createAccesToken } from '../libs/jwt.js'//Modulo para crear un token
 
 
 export const register = async (req, res) => {
@@ -23,7 +22,7 @@ export const register = async (req, res) => {
             username: userSaved.username,
             email: userSaved.email,
             createadAt: userSaved.createdAt,//Registrar fecha de creación
-            createadAt: userSaved.updatedAt,//Registrar fecha de actualización
+            updatedAt: userSaved.updatedAt,//Registrar fecha de actualización
         })
         // res.json(userSaved)mostrar en json todos los datos guardados
         // res.send('Usuario registrado con éxito'); mostrar un simplemensaje al momento de registrar usuario
@@ -34,10 +33,36 @@ export const register = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send('Login');
-};
+export const login = async (req, res) => {
+    const { email, password } = req.body;
 
+    try {
+
+        const userFound = await User.findOne({ email })//Comprobar si el email existe
+        if (!userFound) return res.status(400).json({ message: "user not found" })//Si el email no es encontrado responder con el codigo de estado 400
+
+        const isMatch = await bcrypt.compare(password, userFound.password) //Comprobar si la contraseña es correcta
+        if (!isMatch) return res.status(400).json({ message: 'incorrect password' })//Si la coontraseña es incorrecta responder con un codigo de estado 400
+
+        const token = await createAccesToken({ id: userFound._id })//Del usuario encontrado tomar su id y crear un token
+
+
+        res.cookie('token', token)//Guardar el token en una cookie
+        res.json({//Mostrar datos especificos al momento de hacer un post
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createadAt: userFound.createdAt,//Registrar fecha de creación
+            updatedAt: userFound.updatedAt,//Registrar fecha de actualización
+        })
+        // res.json(userSaved)mostrar en json todos los datos guardados
+        // res.send('Usuario registrado con éxito'); mostrar un simplemensaje al momento de registrar usuario
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
+};
 // La estructura y representación de cómo se organizan los datos y cuáles son las reglas de validación al momneto  de registrar un usuario, se definio en un schema (archivo user.model.js)
 
 
--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -4,19 +4,18 @@ import { TOKEN_SECRET } from '../confg.js'
 export const createAccesToken = (payload) => {
     return new Promise((resolve, reject) => {
         jwt.sign(
-            payload,
-            TOKEN_SECRET,
+            payload,            // Información que se incluirá en el token
+            TOKEN_SECRET,       // Clave secreta para firmar el token (almacenada de forma segura)
             {
-                expiresIn: '1d'
+                expiresIn: '1d' // Duración del token antes de expirar (1 día)
             },
             (err, token) => {
-                if (err) reject(err)//Si algo sale mal arrojar el error
-                resolve(token)//si todo sale bien genera el token
+                if (err) reject(err); // Si ocurre un error en la creación del token, rechaza la promesa con el error
+                resolve(token);       // Si el token se genera correctamente, resuelve la promesa con el token
             }
-        )
-    })
-
-}
+        );
+    });
+};
 
 // header:Contiene el tipo de token y el algoritmo de firma.
 // payload:Contiene las declaraciones sobre el usuario y otros datos relevantes.
